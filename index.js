f490bbd2db2f7c899ffcea769f52e6fd55578bb9
Naama T Gouti <egouti2@gmail.com>
2018-09-21 23:48:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var combined = [];
+   for (var i = 0; i < musicians.length; i++){
+    var combinedString = musicians[i] + " plays " + instruments[i];
+    combined.push(combinedString);
+  }
+  return combined;
+}
+
+function johnLennonFacts(facts) {
+  var arr = [];
+  var x = 0;
+  
+  while (x < facts.length) {
+
+  arr.push(facts[x] + "!!!");
+  x++;
+  }
+  return arr;
+}
